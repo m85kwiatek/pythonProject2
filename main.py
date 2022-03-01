a6f9a0987096edba6d10bdca8250c46d3c8acf6c
mkwiatek <m85kwiatek@gmail.com>
2022-03-01 15:23:28 +0100

    Start1

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ wiek = int(input('ile masz lat? '))
 if wiek >= 18:
     print(name + ' jesteś pełnoletni!')
 else:
-    print('Jesteś jeszcze za młody')
+    print(' Jesteś jeszcze za młody')
 
 # for z pominięciem parzystych
-lista = [1,2,2,3,4,55,6,7,8,9]
+lista = [1, 2, 2, 3, 4, 55, 6, 7, 8, 9]
 lista2 = []
 for x in lista:
-    if x%2 ==0:
+    if x % 2 == 0:
         continue
     else:
         print(x)
-#Pętla while
-x = 1
-while x < 10:
-    if x%2 == 0:
-        print(str(x) + " is even")
-    else:
-        print(str(x) + " is odd")
 
-    x += 1
+# While
+x = 0
+while x <= 5:
+    print(x)
+    x = x+1
+
+# RANGE
+number = 0
+
+for number in range(10):
+    if number == 5:
+        pass    # pass here
+
+    print('Number is ' + str(number))
+
+print('Out of loop')
