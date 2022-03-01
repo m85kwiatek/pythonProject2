200e1daac7715f221b03612a1f4c03b52510385c
mkwiatek <m85kwiatek@gmail.com>
2022-03-01 15:31:14 +0100

    Start1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ else:
     print(' Jesteś jeszcze za młody')
 
 # for z pominięciem parzystych
-lista = [1, 2, 2, 3, 4, 55, 6, 7, 8, 9]
+lista = [1, 2, 2, 3, 4, 55, 6]
 lista2 = []
 for x in lista:
     if x % 2 == 0:
@@ -25,10 +25,24 @@ while x <= 5:
 # RANGE
 number = 0
 
-for number in range(10):
+for number in range(4):
     if number == 5:
         pass    # pass here
 
     print('Number is ' + str(number))
 
 print('Out of loop')
+
+# Continue
+i = 0
+while i<5:
+  i += 1
+  if i==3:
+    print("Skipping 3")
+    continue
+  print(i)
+
+  # listowanie listy
+  words = ["hello", "world", "spam", "eggs"]
+  for word in words:
+      print(word + "!")
